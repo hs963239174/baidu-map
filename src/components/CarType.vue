e88f5912ab09ee05cc5b42337ebe373e07e1b535
hs963239174 <33830024+hs963239174@users.noreply.github.com>
2017-11-20 20:51:03 +0800

    [问题描述]删除无用文件

--- src/components/CarType.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <form class="form-search">
-      <input class="input-medium search-query" type="text" value="請輸入車牌"/>
-      <button type="submit" class="btn">确定</button>
-    </form>
-  </div>
-</template>
--- src/components/TypeLabel.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="span12">
-    <span class="label">1111</span>
-  </div>
-</template>
