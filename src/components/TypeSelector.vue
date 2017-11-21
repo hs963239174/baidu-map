9832da5d742b1dda0dfac4bedb1f769ce80dd506
hs963239174 <hs963239174@icloud.com>
2017-11-21 17:30:18 +0800

    [问题描述]1.增加点击Tag可删除 2：增加搜索警告

--- src/components/TypeSelector.vue
+++ src/components/TypeSelector.vue
@@ -2,6 +2,7 @@
 <template>
   <div id="typeSelector">
     <div class="col-lg-1 col-md-1 col-sm-4" >
+
       <div class="panel-heading" role="tab" id="heading2"  >
         <a data-toggle="collapse" data-parent="" href="" aria-expanded="false"
            aria-controls="collapse3" class="collapsed">
@@ -9,6 +10,7 @@
           <i id="selectRight" class="icon-chevron-right icon-2x"></i>
         </a>
       </div>
+
     <aside  id="selectControl" class="sidebar  perfectScrollbar">
       <div id="solso-sidebar" style="top:0px;">
         <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
@@ -53,8 +55,8 @@
                  aria-labelledby="heading4" aria-expanded="false">
               <div>
                 <form class="form-search list-group-item">
-                  <input class="input-medium search-query" type="text" value="請輸入車牌"/>
-                  <button class="btn">确定</button>
+                  <input id="carLisence" class="input-medium search-query" type="text" flag="0" value="车牌号"/>
+                  <i id="addCarLisence" class="addType icon-plus icon-large" flag="0"></i>
                 </form>
               </div>
             </div>
@@ -74,12 +76,14 @@
                  aria-labelledby="heading5" aria-expanded="false">
               <div>
                 <div class="input-append date form_datetime list-group-item">
-                  <input size="16" type="text" value="开始时间" readonly>
+                  <input id="startTime" size="16" type="text" flag="0" value="开始时间" readonly>
                   <span class="add-on"><i class="icon-th"></i></span>
+                  <i id="addStartTime" class="addType icon-plus icon-large" flag="0"></i>
                 </div>
                 <div class="input-append date form_datetime list-group-item">
-                  <input size="16" type="text" value="结束时间" readonly>
+                  <input id="endTime" size="16" type="text" flag="0" value="结束时间" readonly>
                   <span class="add-on"><i class="icon-th"></i></span>
+                  <i id="addEndTime" class="addType icon-plus icon-large" flag="0"></i>
                 </div>
               </div>
             </div>
@@ -115,36 +119,38 @@
         </div>
       </div>
     </aside>
+
       <div class="modal fade" id="searchCommit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title" id="myModalLabel">确认你的搜索条件</h4>
+              <h4 class="modal-title" id="myModalLabel">确认搜索条件(点击标签可删除)</h4>
             </div>
-            <div id="searchBy" class="modal-body">
-
+            <div class="modal-body">
+              <div id="searchBy" class="row"></div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <button id="searchClose" type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+              <button id="searchBegin" type="button" class="btn btn-primary">搜索</button>
             </div>
           </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
 
-
 <script>
   export default {
     mounted: function() {
-
+      //日期插件初始化
       $(".form_datetime").datetimepicker({
-        format: "yyyy-mm-dd hh:ii"
+        format: "yyyy-mm-dd hh:ii:ss"
       });
+
 
       //选择栏初始化
       $("#selectControl").css("left","0px");
@@ -165,26 +171,94 @@
         $("#selectControl").css("left","0px");
       });
 
-      //选择类型
+      //点击可删除搜索标签
+      $("#searchBy").on("click","span",function () {
+        var targetID = event.target.id;
+        $("#"+targetID).remove();
+        var id = targetID.split("_")[0];
+        var obj = $("#"+id);
+        obj.attr("style","color:white");
+        obj.attr("flag","0");
+
+      });
+
+      //添加搜索类型
       $(".addType").click (function(){
         var id = event.target.id;
-        var obj = document.getElementById(id);
+        var obj =  $("#"+id);
         var targetID = id+"_1";
-        if(obj.getAttribute("flag") == 0)//未添加
+        var value = "";
+
+        if(obj.attr("flag") == 0)//未添加
         {
-          obj.setAttribute("style","color:red");
-          obj.setAttribute("flag","1");
+          obj.attr("style","color:red");
+          obj.attr("flag","1");
+
+          //添加搜索条件标签
+          if(id == "addStartTime")
+          {
+            value = "<span id='"+targetID+"' class='label badge-important'>"+$("#startTime").val()+"</span>";
+            $("#searchBy").append(value);
+          }
+          else if(id == "addEndTime")
+          {
+            value = "<span id='"+targetID+"' class='label badge-important'>"+$("#endTime").val()+"</span>";
+            $("#searchBy").append(value);
+          }
+          else if(id == "addCarLisence")
+          {
+            value = "<span id='"+targetID+"' class='label badge-important'>"+$("#carLisence").val()+"</span>";
+            $("#searchBy").append(value);
+          }
+          else
+          {
+            value = "<span id='"+targetID+"' class='label badge-important'>"+$("#"+id).attr("value")+"</span>";
+            $("#searchBy").append(value);
+          }
         }
-        else if(obj.getAttribute("flag") == 1)//已添加
+        else if(obj.attr("flag") == 1)//已添加
         {
-          obj.setAttribute("style","color:white");
-          obj.setAttribute("flag","0");
+          obj.attr("style","color:white");
+          obj.attr("flag","0");
+
+          //移除搜索条件标签
+          $("#"+targetID).remove();
         }
       });
 
-      $('#searchCommit').on('shown.bs.modal', function () {
-      })
+      //开始搜索
+      $("#searchBegin").click(function () {
+        var searchFlag = $("#searchBy").children().length;
+        if(searchFlag <= 0)
+        {
+          var value = '<div id="alert-search" class="alert-search row alert alert-danger alert-dismissible fade in" role="alert">\
+                          <button type="button" class="close"  aria-label="Close" data-dismiss="alert">\
+                          <span aria-hidden="true">×</span></button>\
+                        <strong>提示：</strong> 未选择任何标签.\
+                        </div>';
+          $(".modal-footer").prepend(value);
+          $("#alert-search").animate({opacity:"0.3"},5000,"swing",function () {
+            $("#alert-search").remove();
+          });
+        }
+        else
+        {
+          //启动搜索
 
+        }
+      });
+
+      //显示模态框回调
+      $('#searchCommit').on('shown.bs.modal', function () {
+
+      });
+
+      //关闭模态框回调
+      $('#searchCommit').on('hidden.bs.modal', function () {
+        //清除警告信息
+        $(".alert-search").stop(true,true);
+        $(".alert-search").remove();
+      })
     }
   }
 
